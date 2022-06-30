91748d5af5472b477dddfa537bde0f9046dab691
popovdmitrij <dima_it@qmotors.ru>
2022-06-30 20:55:03 +0300

    master - quiz

---
+++ public/js/indigo-quiz.js
@@ -0,0 +1,119 @@
+// $(document).ready(function(){
+    var indigo_quiz_step = 1;
+    var indigo_quiz_page = 7;
+    var percent = 0;
+    var indigo_quiz_select = [];
+
+
+    // $(".indigo_quiz_card").click(function () {
+    //     alert("true");
+    // });
+
+    $('.indigo_quiz_card').click(function() {
+        indigo_quiz_select[$('#page_' + indigo_quiz_step).attr('data-title')] = $(this).attr('data-select');
+        // console.log(indigo_quiz_select);
+        hidden_page(indigo_quiz_step);
+        indigo_quiz_step ++;
+        show_page(indigo_quiz_step);
+        change_percent();
+        // console.log(indigo_quiz_select);
+    });
+
+    $('#revert_button').click(function() {
+        if(indigo_quiz_step != 1) {
+            hidden_page(indigo_quiz_step);
+            indigo_quiz_step--;
+            show_page(indigo_quiz_step);
+            change_percent();
+        }
+    });
+
+    $('#indigo_quiz_submit').click(function() {
+        if($('#indigo_quiz_name').val().length == 0){
+            alert('Укажите ваше имя');
+        }
+
+        if($('#indigo_quiz_phone').val().length < 16){
+            alert('Укажите правильный номер телефона');
+        }
+
+        if($('#indigo_quiz_name').val().length != 0 && $('#indigo_quiz_phone').val().length == 16) {
+            hidden_page(indigo_quiz_step);
+            indigo_quiz_step++;
+            show_page(indigo_quiz_step);
+            change_percent();
+            indigo_quiz_select['name'] = $('#indigo_quiz_name').val();
+            indigo_quiz_select['phone'] = $('#indigo_quiz_phone').val();
+            // console.log(indigo_quiz_select);
+
+            $.ajax({
+                url: "/callback_form_quiz",
+                type: "POST", //метод отправки
+                dataType: "html", //формат данных
+
+                data: {
+                    'phone': indigo_quiz_select['phone'],
+                    'name': indigo_quiz_select['name'],
+                    'mark': indigo_quiz_select['Модель'],
+                    'offer': indigo_quiz_select['Услуга'],
+                    'age': indigo_quiz_select['Лет транспорту'],
+                    'probeg': indigo_quiz_select['Пробег'],
+                    'date': indigo_quiz_select['Дата ремонта'],
+                    'zapchasti': indigo_quiz_select['Запчасти'],
+
+
+
+
+                },
+                success: function (response) { //Данные отправлены успешно
+                    //$('.callback-popup-body').html('<p style="text-align: center; font-weight: 700; padding: 20px;">Спасибо! Ваша заявка отправлена.</p>');
+                    // $.magnificPopup.close();
+                    console.log(response);
+                    // window.location.replace("/confirm");
+                },
+                error: function (response) { // Данные не отправлены
+                    console.log('error');
+                }
+            });
+
+        }
+    });
+
+    function hidden_page(e) {
+        $('#page_' + e).addClass('indigo_quiz_page_opacity');
+        $('#header_' + e).addClass('indigo_quiz_page_opacity');
+        setTimeout(() => remove_page(e), 500);
+    }
+
+    function remove_page(e){
+        $('#page_' + e).addClass('indigo_quiz_page_hidden');
+        $('#header_' + e).addClass('indigo_quiz_page_hidden');
+    }
+
+    function show_page(e) {
+        if(e <= indigo_quiz_page) {
+            $('#page_' + e).removeClass('indigo_quiz_page_opacity');
+            $('#header_' + e).removeClass('indigo_quiz_page_opacity');
+            setTimeout(() => $('#page_' + e).removeClass('indigo_quiz_page_hidden'), 500);
+            setTimeout(() => $('#header_' + e).removeClass('indigo_quiz_page_hidden'), 500);
+        } else {
+            $('#page_' + e).removeClass('indigo_quiz_page_opacity');
+            $('#header_' + e).removeClass('indigo_quiz_page_opacity');
+            setTimeout(() => $('#page_thanks').removeClass('indigo_quiz_page_hidden'), 500);
+            setTimeout(() => $('.indigo_quiz').addClass('indigo_quiz_page_opacity'), 2000);
+            setTimeout(() => $('.indigo_quiz').addClass('indigo_quiz_page_scroll'), 2500);
+            setTimeout(() => $('.indigo_quiz').addClass('indigo_quiz_page_hidden'), 3100);
+
+        }
+    }
+    
+    function change_percent() {
+        var indigo_quiz_step_local = indigo_quiz_step;
+        indigo_quiz_step_local --;
+        percent = Math.floor((100 / indigo_quiz_page) * indigo_quiz_step_local);
+        $('#indigo_quiz_progress').width(percent + '%');
+        $('#indigo_quiz_progress_percent').text(percent + '%');
+    }
+
+
+// }
--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -80,4 +80,77 @@ class MailerController extends AbstractController
 
         return new JsonResponse(['success'=>'<p>Спасибо! Ваша заявка отправлена.</p>']);
     }
+
+    /**
+     * @Route("/callback_form_quiz", name="callback_form_quiz")
+     */
+    public function callback_form_quiz(Request $request, MailerInterface $mailer){
+        $token = "2102312578:AAF6iR_1pAUR4GY1Vg8TwgF3CsIBCKWQyBg";
+        $chat_id = "-1001677654724";# Заявки VAG-PIK
+
+        $arr = array(
+            "Заявка с" => " с квиза сайта https://audi.pikms.ru/ ",
+            "Телефон" => $request->get('phone'),
+            "Имя" => $request->get('name'),
+            "Марка" => $request->get('mark'),
+            "Услуга" => $request->get('offer'),
+            "Лет" => $request->get('age'),
+            "Пробег" => $request->get('probeg'),
+            "Запчасти" => $request->get('zapchasti'),
+            "Дата ремонта" => $request->get('date'),
+        );
+        /*Цикл по массиву (собираем сообщение) */
+        $txt = '';
+        foreach($arr as $key => $value) {
+            $txt .= "<b>".$key."</b>: ".htmlspecialchars($value)."\n";
+        }
+        $sendTextToTelegram = file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&parse_mode=html&text=".rawurlencode($txt))."\n";
+        if (!$sendTextToTelegram){
+            return new JsonResponse(['error'=>'<p>Ошибка при отправке в Telegram</p>']);
+        }
+
+        /* $to = 'info@piksp.ru';
+
+         $email = (new Email())
+             ->from('info@my-side.online')
+             ->to((string)$to)
+             ->subject('Новая заявка с сайта Piksp.ru')
+             ->html('<p>Новая заявка с сайта Piksp.ru</p>
+             <p>Телефон отправителя: ' . $request->get('phone') . '</p>'
+             );
+         $mailer->send($email);*/
+
+        //Roistat
+        $roistatData = array(
+            'roistat' => isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : 'nocookie',
+            'key'     => 'ODUxOTY2ZGIxZTAzOWRlNGU0M2IwYTBlOTgzNDczYzI6MTE2MDU4', // Ключ для интеграции с CRM, указывается в настройках интеграции с CRM.
+            'title'   => 'Заявка с формы сайта https://audi.pikms.ru/', // Название сделки
+            'comment' => 'Марка => ' . $request->get('mark') . ' | Услуга => ' .  $request->get('offer') . ' | Лет авто => ' . $request->get('age') . ' | Пробег => ' . $request->get('probeg') . ' | Запчасти => ' . $request->get('zapchasti') . ' | Дата ремонта => ' . $request->get('date'), // Комментарий к сделке
+            'name'    => $request->get('name'), // Имя клиента
+            'email'   => '', // Email клиента
+            'phone'   => $request->get('phone'), // Номер телефона клиента
+            'order_creation_method' => '', // Способ создания сделки (необязательный параметр). Укажите то значение, которое затем должно отображаться в аналитике в группировке "Способ создания заявки"
+            'is_need_callback' => '0', // Если указано значение '1', на номер клиента будет инициироваться обратный звонок после создания заявки в Roistat (независимо от того, включен ли обратный звонок в Ловце лидов). Если указано значение '0', для данной формы обратный звонок инициироваться не будет (даже если в Ловце лидов включен обратный звонок).
+            'callback_phone' => '', // Переопределяет номер, указанный в настройках обратного звонка.
+            'sync'    => '0', //
+            'is_need_check_order_in_processing' => '1', // Включение проверки заявок на дубли
+            'is_need_check_order_in_processing_append' => '1', // Если создана дублирующая заявка, в нее будет добавлен комментарий об этом
+            'is_skip_sending' => '0', // Не отправлять заявку в CRM.
+            'fields'  => array(
+                'Адрес' => 'И31АС4',
+                'Марка'    => $request->get('mark'),
+//                'Модель'   => '-',
+                'Сайт'    => 'audi.pikms.ru',
+                // Массив дополнительных полей. Если дополнительные поля не нужны, оставьте массив пустым.
+                // Примеры дополнительных полей смотрите в таблице ниже.
+                // Помимо массива fields, который используется для сделки, есть еще массив client_fields, который используется для установки полей контакта.
+//                "charset" => "Windows-1251", // Сервер преобразует значения полей из указанной кодировки в UTF-8.
+            ),
+        );
+
+        file_get_contents("https://cloud.roistat.com/api/proxy/1.0/leads/add?" . http_build_query($roistatData));
+
+        return new JsonResponse(['success'=>'<p>Спасибо! Ваша заявка отправлена.</p>']);
+    }
+
 }
